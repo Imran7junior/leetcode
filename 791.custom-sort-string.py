1d07caac9123c209490bbb7c09c004f23518f47f
MD Imran Mansuri <md.imran.568@gmail.com>
2024-03-12 00:47:51 +0530

    mar10

---
+++ 791.custom-sort-string.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=791 lang=python3
+#
+# [791] Custom Sort String
+#
+
+# @lc code=start
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        char_count = {char: 0 for char in order}
+        for char in s:
+            if char in char_count:
+                char_count[char] += 1
+    
+        sorted_s = ''
+        for char in order:
+            sorted_s += char * char_count[char]
+    
+        for char in s:
+            if char not in order:
+                sorted_s += char
+
+        return sorted_s
+# @lc code=end
+
