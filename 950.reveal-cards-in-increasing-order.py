13f7a2c7102f117025b63422951f9f7a169c88da
MD Imran Mansuri <md.imran.5682@gmail.com>
2024-04-11 17:59:35 +0530

    april10

--- 950.reveal-cards-in-increasing-order.py
+++ 950.reveal-cards-in-increasing-order.py
@@ -7,6 +7,17 @@
 # @lc code=start
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        deck.sort()
+        res = [9] * len(deck)
+        q = deque(range(len(deck)))
+        
+        for n in deck:
+            i = q.popleft()
+            res[i] = n
+            if q:
+                q.append(q.popleft())
+        
+        return res
         
 # @lc code=end
 
