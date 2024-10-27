6eb6d9f10888fb3efcf175e53f944c87bcd6480a
MD Imran Mansuri <md.imran.5682@gmail.com>
2024-10-28 02:26:26 +0530

    oct27

---
+++ 554.brick-wall.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=554 lang=python3
+#
+# [554] Brick Wall
+#
+
+# @lc code=start
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        
+# @lc code=end
+
