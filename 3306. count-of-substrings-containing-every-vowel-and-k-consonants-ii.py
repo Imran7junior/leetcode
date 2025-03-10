2dc23083bbbc539129a79afab1ede591ec2c7b37
MD Imran <md.imran.5682@gmail.com>
2025-03-10 20:15:38 +0530

    mar10

--- 3306. count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3306. count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -1,30 +1,36 @@
-def countOfSubstrings(word: str, k: int) -> int:
-    return atleast(k, word) - atleast(k + 1, word)
+class Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        frequencies = [{}, {}]
+        for v in "aeiou":
+            frequencies[0][v] = 1
+        
+        response, currentK, vowels, extraLeft, left = 0, 0, 0, 0, 0
 
-def atleast(k, word):
-    count = consonants = left = 0
-    freq = {}
-    n = len(word)
-    vowels = {'a', 'e', 'i', 'o', 'u'}
+        for right, rightChar in enumerate(word):
+            if rightChar in frequencies[0]:
+                frequencies[1][rightChar] = frequencies[1].get(rightChar, 0) + 1
+                if frequencies[1][rightChar] == 1:
+                    vowels += 1
+            else:
+                currentK += 1
 
-    for right in range(n):
-        ch = word[right]
-        if ch not in vowels:
-            consonants += 1
+            while currentK > k:
+                leftChar = word[left]
+                if leftChar in frequencies[0]:
+                    frequencies[1][leftChar] -= 1
+                    if frequencies[1][leftChar] == 0:
+                        vowels -= 1
+                else:
+                    currentK -= 1
+                left += 1
+                extraLeft = 0
 
-        freq[ch] = freq.get(ch, 0) + 1
+            while vowels == 5 and currentK == k and left < right and word[left] in frequencies[0] and frequencies[1][word[left]] > 1:
+                extraLeft += 1
+                frequencies[1][word[left]] -= 1
+                left += 1
 
-        while consonants >= k and hasAllVowels(freq):
-            count += (n - right)
-            leftChar = word[left]
-            if leftChar not in vowels:
-                consonants -= 1
-            freq[leftChar] -= 1
-            if freq[leftChar] == 0:
-                del freq[leftChar]
-            left += 1
+            if currentK == k and vowels == 5:
+                response += (1 + extraLeft)
 
-    return count
-
-def hasAllVowels(freq):
-    return all(freq.get(v, 0) > 0 for v in "aeiou")
+        return response
