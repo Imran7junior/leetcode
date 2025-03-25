8ffa6f6f2836b4c90b05e6308be99d8b75f078a5
MD Imran <md.imran.5682@gmail.com>
2025-03-25 22:35:37 +0530

    25mar

--- 3169. count-days-without-meetings.py
+++ 3169. count-days-without-meetings.py
@@ -1,7 +1,10 @@
 class Solution:
-    def countDays(self, days: int, nums: list[list[int]]) -> int:
-        visited = [False] * days
-        for start, end in nums:
-            for j in range(start, end + 1):
-                visited[j - 1] = True
-        return visited.count(False)
+    def countDays(self, days: int, meetings: list[list[int]]) -> int:
+        meetings.sort()
+        unvisited, temp = 0, 1
+        for start, end in meetings:
+            unvisited += max(0, start - temp)
+            temp = max(temp, end + 1)
+            if temp > days:
+                break
+        return unvisited + max(0, days - temp + 1)
