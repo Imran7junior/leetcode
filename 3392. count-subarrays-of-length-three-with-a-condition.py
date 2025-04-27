21bdf3936af87522b02875310e74de6329589ad3
MD Imran <md.imran.5682@gmail.com>
2025-04-27 13:40:19 +0530

    apr27

--- 3392. count-subarrays-of-length-three-with-a-condition.py
+++ 3392. count-subarrays-of-length-three-with-a-condition.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
 	def countSubarrays(self, nums: List[int]) -> int:
 		n = len(nums)
@@ -6,3 +7,8 @@ class Solution:
 			if nums[i] == (nums[i-1] + nums[i+1]) * 2:
 				ans += 1
 		return ans
+	
+sol = Solution()
+nums = [1, 2, 1, 4, 1]  # <-- you can change this list
+result = sol.countSubarrays(nums)
+print("Result:", result)
