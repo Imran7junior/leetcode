9f634db499817dd58dcac318d2832d13409f6612
MD Imran <md.imran.5682@gmail.com>
2025-12-26 21:06:54 +0530

    dec26

---
+++ 2483. minimum-penalty-for-a-shop.py
@@ -0,0 +1,18 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        bestTime = 0
+        minPenalty = 0
+        prefix = 0
+        
+        for i in range(len(customers)):
+            prefix += -1 if customers[i] == 'Y' else 1
+            
+            if prefix < minPenalty:
+                bestTime = i + 1
+                minPenalty = prefix
+        
+        return bestTime
+# Test case
+customers = "YYNY"
+sol = Solution()
+print(sol.bestClosingTime(customers))  # Expected: 2
