a121be7980ecb3c7b85185529103cc7578cb62f0
MD Imran <md.imran.5682@gmail.com>
2026-01-20 12:07:26 +0530

    s

--- 3314. construct-the-minimum-bitwise-array-i.java
+++ 3314. construct-the-minimum-bitwise-array-i.java
@@ -3,11 +3,11 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    public int[] minBitwiseArray(int[] nums) {
-        int[] res = new int[nums.length];
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int[] res = new int[nums.size()];
         
-        for (int i = 0; i < nums.length; i++) {
-            int n = nums[i];
+        for (int i = 0; i < nums.size(); i++) {
+            int n = nums.get(i);
             if ((n & 1) == 1) {
                 res[i] = n & ~(((n + 1) & ~n) >> 1);
             } else {
@@ -23,13 +23,13 @@ class Solution {
         Solution solution = new Solution();
         
         // Test case 1
-        int[] nums1 = {2, 3, 5, 7};
+        List<Integer> nums1 = Arrays.asList(2, 3, 5, 7);
         int[] output1 = solution.minBitwiseArray(nums1);
         System.out.println("Test 1: nums = [2,3,5,7]");
         System.out.println("Output: " + Arrays.toString(output1) + ", Expected: [-1,1,4,3]");
         
         // Test case 2
-        int[] nums2 = {11, 13, 31};
+        List<Integer> nums2 = Arrays.asList(11, 13, 31);
         int[] output2 = solution.minBitwiseArray(nums2);
         System.out.println("Test 2: nums = [11,13,31]");
         System.out.println("Output: " + Arrays.toString(output2) + ", Expected: [9,12,15]");
