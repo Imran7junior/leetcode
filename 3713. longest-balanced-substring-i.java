f9bf6b64a386b76499723c643a33fefe248c3425
MD Imran <md.imran.5682@gmail.com>
2026-02-12 11:13:41 +0530

    feb12

---
+++ 3713. longest-balanced-substring-i.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int longestBalanced(String s) {
+        int n = s.length();
+
+        // Transform char -> int
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) 
+            a[i] = s.charAt(i) - 'a';
+
+        int result = 0;
+        for (int l = 0; l < n; l++) {
+            // Early exit, can't be bigger
+            if (n - l <= result) 
+                break;
+
+            int[] cnt = new int[26]; // Counts of every char
+            int uniq = 0, maxfreq = 0; // Number of uniq chars and maximum frequency
+            for (int r = l; r < n; r++) {
+                int i = a[r];
+
+                // There was no this char before => one more uniq
+                if (cnt[i] == 0) 
+                    uniq++;
+
+                cnt[i]++;
+                // Update max frequency
+                if (cnt[i] > maxfreq) 
+                    maxfreq = cnt[i];
+
+                // Check if all uniq chars have maxfreq frequency then update the result
+                int cur = r - l + 1;
+                if (uniq * maxfreq == cur && cur > result)
+                    result = cur;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        // Example 1
+        String s1 = "abbac";
+        System.out.println(sol.longestBalanced(s1));  // Expected: 4
+        
+        // Example 2
+        String s2 = "zzabccy";
+        System.out.println(sol.longestBalanced(s2));  // Expected: 4
+        
+        // Example 3
+        String s3 = "aba";
+        System.out.println(sol.longestBalanced(s3));  // Expected: 2
+    }
+}
