3c82856e4bfc16183bfc0c3653436f767926df8f
MD Imran <md.imran.5682@gmail.com>
2026-06-16 11:55:18 +0530

    jun16

---
+++ 3612. process-string-with-special-operations-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder ans = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ch == '*' && ans.length() >= 1) {
+                ans.deleteCharAt(ans.length() - 1);
+            } else if (ch == '#' && ans.length() >= 1) {
+                String temp = ans.toString();
+                ans.append(temp);
+            } else if (ch == '%') {
+                ans.reverse();
+            }
+            if (ch >= 'a' && ch <= 'z') {
+                ans.append(ch);
+            }
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.processStr("a#b%*")); // "ba"
+        System.out.println(sol.processStr("z*#"));   // ""
+    }
+}
