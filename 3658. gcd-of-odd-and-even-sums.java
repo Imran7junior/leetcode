1d9fb574ba6b1340b0e4bd24b09dca57d5ba9a0c
MD Imran <md.imran.5682@gmail.com>
2026-07-15 12:24:18 +0530

    jul15

---
+++ 3658. gcd-of-odd-and-even-sums.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        // Example 1
+        int n1 = 4;
+        System.out.println(sol.gcdOfOddEvenSums(n1)); // 4
+
+        // Example 2
+        int n2 = 5;
+        System.out.println(sol.gcdOfOddEvenSums(n2)); // 5
+    }
+}
---
+++ 3658. gcd-of-odd-and-even-sums.py
@@ -0,0 +1,15 @@
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        return n
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    # Example 1
+    n = 4
+    print(sol.gcdOfOddEvenSums(n))  # 4
+
+    # Example 2
+    n = 5
+    print(sol.gcdOfOddEvenSums(n))  # 5
